74a9c191fe6e48fc010d5025528ff5f012346de8
Priya <nilapriya5517@gmail.com>
2022-02-07 20:55:13 +0530

    commit on main

--- src/components/respons_nav/respons_nav.js
+++ src/components/respons_nav/respons_nav.js
@@ -4,29 +4,51 @@ function Respons_nav() {
 
   const addclass = () => {
     document.querySelector("#btn").classList.toggle("inactive");
-
     document.querySelector("#sidebar").classList.toggle("close");
+    document.querySelector("#logo").classList.toggle("hidden");
+    document.querySelector("#logo_1").classList.toggle("hidden");
+    document.querySelector("#profile").classList.toggle("profile_1");
 
-    const hide = document.querySelectorAll("#logo");
-    for (let i = 0; i < hide.length; i++) {
-        hide[i].classList.toggle("hidden");
-      }
+
+
+     const hide = document.querySelectorAll("#hide");
+   for (let i = 0; i < hide.length; i++) {
+        hide[i].classList.toggle("profilecnt_none");
+       }
+  };
+  const addfnt = ()=>{
+    
+    
+    document.querySelector("#logo").classList.remove("hidden"); 
+    document.querySelector("#logo_1").classList.remove("hidden");
+    document.querySelector("#btn").classList.remove("inactive"); 
+    document.querySelector("#sidebar.close").classList.toggle("close");
+    document.querySelector("#profile").classList.toggle("profile_1");
+
+
+   
+
+    const show = document.querySelectorAll("#hide");
+    for (let i = 0; i < show.length; i++) {
+         show[i].classList.remove("profilecnt_none");
+        }
   };
 
   return (
     <>
       <div className="sidebar" id="sidebar">
-        <div className="logo_cnt" >
+        <div className="logo_cnt">
           <div className="logo">
             <i  class="lg fa fa-code" id='logo'></i>
-            <span className="logo_name" id='logo' >CodingLab</span>
+            <span className="logo_name" id='logo_1' >CodingLab</span>
           </div>
-          <i onClick={addclass} id="btn" className=" btn fa fa-bars"></i>
+          <i onClick={addclass} id="btn" className="btn fa fa-bars"></i>
         </div>
         <ul className="nav_list">
           <li>
             <a href="#">
-              <i  class=" search fa fa-search"></i>
+            
+              <i  onClick={addfnt} id="bar" class=" search fa fa-search"></i>
               <input type="text" placeholder="Search..."></input>
             </a>
             <span id="tooltips" className=" tooltips">
@@ -51,21 +73,14 @@ function Respons_nav() {
             </a>
             <span id="tooltips" className="tooltips">Message</span>
           </li>
-         {/*  <li>
+           <li>
             <a href="#">
               <i class="fa fa-calendar"></i>
               <span className="link_name">Analytics</span>
             </a>
             <span className="tooltips">Analytics</span>
           </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-folder"></i>
-              <span className="link_name">File Manager</span>
-            
-            </a>
-            <span className="tooltips">File Manager</span>
-          </li>
+         
           <li>
             <a href="#">
               <i class="fa fa-shopping-cart"></i>
@@ -86,25 +101,26 @@ function Respons_nav() {
               <span className="link_name">Settings</span>
             </a>
             <span className="tooltips">Settings</span>
-          </li> */}
+          </li> 
         </ul>
-        {/* <div className="profile_cnt">
-          <div className="profile">
+         <div className="profile_cnt" >
+          <div className="profile" id="profile">
             <div className="profile_details">
               <img
+      
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSoQFvYAr4KD4S-iecBnmLmPf7zuyFyHkd8w&usqp=CAU"
                 className="pfl"
                 alt=""
               ></img>
              <div className="name_job">
-              <div className="profile_name">PriyaDharshini</div>
-              <div className="job">Web designer</div>
+              <div id="hide" className="profile_name">PriyaDharshini</div>
+              <div id="hide" className="job">Web designer</div>
               </div>
 
-            <i class=" logout fa fa-arrow-left"></i>
+            <i id="hide" class=" logout fa fa-arrow-left"></i>
             </div>
           </div>
-        </div> */}
+        </div> 
       </div>
     </>
   );

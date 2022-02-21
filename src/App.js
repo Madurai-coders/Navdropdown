da7ae3a2502e8c45a5b6186a2d91b17b17eccaf2
Priya <nilapriya5517@gmail.com>
2022-02-21 23:44:58 +0530

    commit on main

--- src/App.js
+++ src/App.js
@@ -28,26 +28,7 @@ function App() {
               <Route path="/settings" excat component={Setting}></Route>
             </Switch>
 
-            {/* {isTabletOrMobile && (
-              <div className="bottom_nav">
-                <a href="#" className="navlist mt-2">
-                  <i class="home fa fa-home" aria-hidden="true"></i>
-                  <span className="nav_txt">Home</span>
-                </a>
-                <a href="#" className="navlist mt-2">
-                  <i class="cart fa fa-cart-plus" aria-hidden="true"></i>
-                  <span className="nav_txt">Cart</span>
-                </a>
-                <a href="#" className="navlist mt-2">
-                  <i class="act fa fa-user" aria-hidden="true"></i>
-                  <span className="nav_txt">User</span>
-                </a>
-                <a href="#" className="navlist mt-2">
-                  <i class=" community fa fa-users" aria-hidden="true"></i>
-                  <span className="nav_txt">Community</span>
-                </a>
-              </div>
-            )} */}
+            
             <Btm_nav></Btm_nav>
           </div>
         </BrowserRouter>
